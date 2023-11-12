ad581d1c21dd55a3aefbfe92df43d3e1c4191085
unknown <rkawk1539@gmail.com>
2023-11-12 15:00:18 +0900

    별들

--- src/component/intro.js
+++ src/component/intro.js
@@ -1,13 +1,22 @@
-import styles from "./style/intro.module.css"
+import styles from "./style/component.module.css";
+import "./style/intro.css";
 import StarShine from "./starShine";
 
 export default function Intro() {
 
+    function stars() {
+        const starComponents = [];
+        for (let i = 0; i < 100; i++) {
+            starComponents.push(<StarShine key={i} id={i} />);
+        }
+        return starComponents;
+    }
+
     return (
-        <div className={`${styles.introContainer} ${styles.darkBackground}`}>
+        <div className={`introContainer ${styles.darkBackground}`}>
             <div className={styles.backdropFilter}>
-                <div className={styles.introMonitor}>
-                    <StarShine></StarShine>
+                <div className="introMonitor">
+                    {stars()} {/* Call the stars function to render the StarShine components */}
                 </div>
             </div>
         </div>
--- src/component/starShine.js
+++ src/component/starShine.js
@@ -1,14 +1,38 @@
 import img from "./style/star.png"
+import { useEffect } from "react"
 
-const starStyle = {
-    width: 10 + Math.floor(Math.random() * 20),
+export default function StarShine({id}) {
+    useEffect(() => {
+        const animationName = `shine`;
+        const animationDuration = `${30 + Math.random() * 20}s`;
+        const animationTimingFunction = `linear`;
+        const animationDelay = `${1 + Math.random() * 2}s`;
 
-}
+        const styleSheet = document.styleSheets[0];
+        styleSheet.insertRule(`
+        @keyframes ${animationName} {
+            0% {
+                transform: translateX(90vw)
+            }
+        }`);
 
-export default function StarShine() {
+        const star = document.getElementById('1');
+        star.style.animation = `${animationName} 1s ${animationTimingFunction}`
+
+        return () => {
+            console.log("Clean!")
+        }
+    }, []);
+
+    const starStyle = {
+        position: 'absolute',
+        width: 10 + Math.floor(Math.random() * 20),
+        opacity: Math.random(),
+        top: `${Math.random() * 99}vh`,
+        left: -50,
+    }
 
     return (
-        <img src={img} style={starStyle} alt="1"></img>
-
+        <img id={id} style={starStyle} src={img} alt="1" />
     )
 }
