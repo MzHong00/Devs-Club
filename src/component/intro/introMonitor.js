e99e01363ba0ad948501d80035a331abf2df24b8
JM <rkawk1539@gmail.com>
2023-11-16 12:55:54 +0900

    11.16

--- src/component/intro/introMonitor.js
+++ src/component/intro/introMonitor.js
@@ -1,18 +1,16 @@
-export default function IntroMonitor() {
+import homeJson from "../../constant/home.json"
 
+export default function IntroMonitor() {
     return (
         <div className="introMonitor">
-            <div className="introText">
-                <div className="clubTitle">
-                    DEVS
-                </div>
-                <div className="clubSubTitle">
-                    우송대학교 IT 융합학부 동아리
+            <div className="introMainFrame">
+                <div className="introText">
+                    <div className="clubTitle">{homeJson.club_name}</div>
+                    <div className="clubSubTitle">{homeJson.introduce}</div>
                 </div>
             </div>
             <div className="introSubFrame">
                 <div className="loginFrame">
-
                 </div>
                 <div className="searchFrame">
                     <div className="searchButton"></div>
---
+++ src/component/intro/navBar.js
@@ -0,0 +1,11 @@
+
+
+export default function SocialBar({}) {
+    
+
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
