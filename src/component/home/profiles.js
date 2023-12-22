2a826dba76319529d61c0a2641ebb04912c241de
JM <rkawk1539@gmail.com>
2023-12-22 22:06:05 +0900

    memberContents Add

---
+++ src/component/home/profiles.js
@@ -0,0 +1,16 @@
+
+
+export default function Profiles() {
+
+    return (
+        <div className="profiles">
+            <div className="memberImg">
+
+            </div>
+            <div className="memberInfo">
+                <div className="name"></div>
+                <div className=""></div>
+            </div>
+        </div>
+    )
+}
